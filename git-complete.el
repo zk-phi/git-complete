92180e5abd63f351a330e1bab049da04f39147fd
p <_>
2018-02-06 13:00:40 +0900

    Tweak default threshold

--- git-complete.el
+++ git-complete.el
@@ -72,7 +72,7 @@ modes."
   :type '(repeat symbol)
   :group 'git-complete)
 
-(defcustom git-complete-threshold 0.005
+(defcustom git-complete-threshold 0.3
   "Threshold to filter the results from `git grep'. When 0.005
 for example, which is the defualt value, completion candidates
 which occupy less than 0.5% amount the grep results are dropped."
